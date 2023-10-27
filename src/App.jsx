0dd04ff1825a228ab0e0cc296ec82a7d4bcac5bb
EDGARS VALKOVSKIS <EDATBIZ@GMAIL.COM>
2023-10-27 15:11:45 +0100

    newfilslssls

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Work from "./pages/Work";
+import Skills from "./pages/Skills";
+import Interests from "./pages/Interests";
+import Hobbies from "./pages/Hobbies";
+import Education from "./pages/Education";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -10,11 +15,14 @@ function App() {
         <h1>React Router Dom</h1>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/pages/work">Work</Link>
+        <Link>to=""</Link>
       </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
       </Routes>
 
       <footer>
@@ -25,3 +33,11 @@ function App() {
 }
 
 export default App;
+
+import PageStructure from "./components/PageStructure";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import ErrorPage from "./pages/404";
+import ThankYou from "./pages/ThankYou";
---
+++ src/pages/Work.jsx
@@ -0,0 +1,24 @@
+export default function Work() {
+  return (
+    <>
+      <h1>Professional Experience</h1>
+
+      <section>
+        <h3 className="Uber">Uber</h3>
+        <p>Food Delivery Courier with a focus on customer service</p>
+        <p>January 2023 – present | Norwich</p>
+        <li>
+          <ul>
+            <p>
+              Connecting with customers to update them on their order status by
+              <br>calling them or leaving a message.</br>
+            </p>
+          </ul>
+        </li>
+      </section>
+    </>
+  );
+}
+</>
+
+
