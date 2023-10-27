30bfc579e70409038ec71c33c1554932a8efe567
EDGARS VALKOVSKIS <EDATBIZ@GMAIL.COM>
2023-10-27 13:17:57 +0100

    verseee

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h2>Home Page</h2>
+        <p>
+          Reacter Router Dom allows us to have multiple pages in our SPA (Single
+          Page Application)
+        </p>
+      </main>
+    </>
+  );
+}
